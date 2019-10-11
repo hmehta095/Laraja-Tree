30574cc134411c9808311a0a226072dbc0e97c7e
himanshu <hmehta095@gmail.com>
2019-10-11 12:17:00 -0400

    orange coming from tree and orange is bouncing

--- LaranjaTree-F19/GameScene.swift
+++ LaranjaTree-F19/GameScene.swift
@@ -11,11 +11,37 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    
+    // MARK: Sprites
+    // --------------------------------------
+    var ground:SKSpriteNode!
+    
+    // ???? orange, tree, ground, wall
+    
+    // WRITE THE CODE TO MAKE AN ORANGE
+    // WHEN PERSON CLICKS ON SCREEN
+    
+    
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
+        
+        
+        // setup ground sprite
+        self.ground = self.childNode(withName: "ground") as! SKSpriteNode
+        
+        self.ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
+        self.ground.physicsBody?.affectedByGravity = false
+        self.ground.physicsBody?.categoryBitMask = 32
+        self.ground.physicsBody?.collisionBitMask = 0
+        self.ground.physicsBody?.contactTestBitMask = 0
+        
+        
+        
+        
+        
     }
     override func update(_ currentTime: TimeInterval) {
-    
+        
     }
     func didBegin(_ contact: SKPhysicsContact) {
         print("collision occured")
@@ -32,11 +58,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         print("Sprite 2: \(nodeB!.name)")
         print("------")
         
-//        if (nodeA!.name == "player" && nodeB!.name == "enemy") {
-//            // player die
-//            print("RESETTING POSITION-AAAA")
-//            restartPlayer()
-//        }
+        //        if (nodeA!.name == "player" && nodeB!.name == "enemy") {
+        //            // player die
+        //            print("RESETTING POSITION-AAAA")
+        //            restartPlayer()
+        //        }
         
         
     }
@@ -50,17 +76,45 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if (mouseTouch == nil) {
             return
         }
-        
         let location = mouseTouch!.location(in: self)
         let mouseXPosition = location.x
         let mouseYPosition = location.y
         
+        
+        // detect what sprite was touched
+        let nodeTouched = atPoint(location).name
+        if (nodeTouched == "tree") {
+            spawnOrange(x: mouseXPosition, y: mouseYPosition)
+        }
+    }
+    
+    // make your orange
+    func spawnOrange(x:CGFloat, y:CGFloat) {
+        
+        // DRAW THE ORANGE
+        //------------------------------
         // 1. make an orange
         let orange = SKSpriteNode(imageNamed: "Orange")
         // 2. Position the orange on the scren
-        orange.position.x = mouseXPosition
-        orange.position.y = mouseYPosition
-        // 3. show the orange on screen
+        orange.position.x = x
+        orange.position.y = y
+        // 3. Force orange to always appear in foreground
+        orange.zPosition = 999
+        
+        // 4. show the orange on screen
         addChild(orange)
+        
+        // ADD PHYSICS TO THE ORANGE
+        // ---------------------------
+        // 1. Give the orange a physics body
+        orange.physicsBody = SKPhysicsBody(circleOfRadius: orange.size.width / 2)
+        // 2. Give orange gravity. By default, orange is affected by gravity.
+        // orange.physicsBody?.affectedByGravity = true
+        // 3. Set category, collision, and contact bit masks
+        //  - By default, collison = everything, contact = 0
+        orange.physicsBody?.categoryBitMask = 1
+        orange.physicsBody?.contactTestBitMask = 0
+        
+        orange.physicsBody?.restitution = 1
     }
 }
